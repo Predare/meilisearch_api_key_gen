9ce2e8bfde1afb8388fdaff19ce3ba10715e11f8
Danil Kassymov <predare12000@gmail.com>
2023-10-08 15:23:05 +0300

    change import of request func

--- src/meilisearch_api_key_gen/keygen.py
+++ src/meilisearch_api_key_gen/keygen.py
@@ -1,4 +1,4 @@
-from request import make_request 
+from meilisearch_api_key_gen.request import make_request
 
 def key_gen(masterKey: str, address: str, actions: list, indexes: list):
     response = make_request(masterKey, address, actions, indexes)
